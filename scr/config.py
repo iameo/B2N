d28f05a8c874ac6b312192b15a0d27cfe6b7188e
Emmanuel Okwudike <34962281+iameo@users.noreply.github.com>
2020-12-27 14:07:02 +0100

    updated secret key(previous was a test key; separate from prod's)

--- scr/config.py
+++ scr/config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = 'xx98cbd764a4fb7bfdc5895e2ae43xrfc'
+    SECRET_KEY = 'enter-new-secret'
     SQLALCHEMY_DATABASE_URI = (os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'bbntwitter.db'))+'?check_same_thread=False'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
